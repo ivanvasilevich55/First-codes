794758801f07c5fd82a5ed4ca10b4a016765fbde
ivanvasilevich55 <ivanvasilevich55@yandex.ru>
2017-02-07 17:09:26 +0500

    first commit

---
+++ urok6.php
@@ -0,0 +1,26 @@
+<?php
+$addition = 2 + 4; 
+$subtraction = 6 - 2; 
+$multiplication = 5 * 3; 
+$division = 16 / 3; 
+$modulus = 5 % 2; 
+echo "Пример сложения: 2 + 4 = ".$addition."<br />"; //ссумирование
+echo "Пример вычитания: 6 - 2 = ".$subtraction."<br />"; // вычитание
+echo "Пример умножения:  5 * 3 = ".$multiplication."<br />"; //умножение
+echo "Пример деления нацело: 16 / 3 = ".$division."<br />";//целая часть
+echo "Пример остатка от деления: 5 % 2 = " . $modulus;# остаток
+echo "\n<br>\n";
+$a = 1;
+$b = 2;
+$c = 3;
+$d = 4;
+$a += 3;// a=a+3//4
+$b -= 3;// b=b-3//-1
+$c *= 2;// c=c*2//6
+$d /= 2;// d=d/2//2
+$counter++;//+1
+echo "a=".$a."\n<br>\n";
+echo "b=".$b."\n<br>\n";
+echo "c=".$c."\n<br>\n";
+echo "d=".$d."\n<br>\n";
+?>
